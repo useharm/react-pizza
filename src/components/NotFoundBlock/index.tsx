938982d60e09428224aebe1ed139bce141b3dba8
useharm <cool.evsey@mail.ru>
2022-10-03 21:11:47 +0300

    set types

---
+++ src/components/NotFoundBlock/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './NotFoundBlock.module.scss';
+
+const NotFoundBlock: React.FC = () => {
+    return (
+        <div className={styles.root}>
+            <h1>
+                <span>😕</span>
+                <br />
+                Ничего не найдено </h1>
+        </div>
+    );
+};
+
+export default NotFoundBlock;
